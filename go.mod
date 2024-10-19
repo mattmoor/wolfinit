4a106bce8bfef188177542a14ebff673c6733826
Matt Moore <mattmoor@chainguard.dev>
2024-10-19 09:56:32 -0700

    Dump clog, set up some of the basics.
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/mattmoor/wolfinit
 
 go 1.23.0
 
-require github.com/chainguard-dev/clog v1.5.0
+require github.com/moby/sys/mount v0.3.4
+
+require (
+	github.com/moby/sys/mountinfo v0.7.2 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//go:build !darwin && !windows
+// +build !darwin,!windows
+
 // Copyright 2024 Chainguard, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
@@ -5,17 +8,69 @@ package main
 
 import (
 	"context"
-	"log/slog"
+	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 
-	"github.com/chainguard-dev/clog"
+	"github.com/moby/sys/mount"
 )
+
+// This is to mimic the following "trap"
+// echo s > /proc/sysrq-trigger && echo o > /proc/sysrq-trigger && sleep infinity
+func shutdown() {
+	// Write 's' to /proc/sysrq-trigger
+	if err := os.WriteFile("/proc/sysrq-trigger", []byte("s\n"), 0644); err != nil {
+		log.Fatalf("failed to sync %v", err)
+	}
+
+	// Write 'o' to /proc/sysrq-trigger
+	if err := os.WriteFile("/proc/sysrq-trigger", []byte("o\n"), 0644); err != nil {
+		log.Fatalf("failed to poweroff %v", err)
+	}
+
+	// Block forever
+	select {}
+}
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
-	ctx = clog.WithLogger(ctx, clog.New(slog.Default().Handler()))
 
-	clog.InfoContext(ctx, "Hello, world!")
+	// mount -t proc proc -o nodev,nosuid,hidepid=2 /proc
+	if err := mount.Mount("proc", "/proc", "proc", "nodev,nosuid,hidepid=2"); err != nil {
+		log.Fatalf("failed to mount: %v", err)
+	}
+	// Once `/proc` is mounted, we can set up the shutdown handler, which writes
+	// to `/proc/sysrq-trigger` to power off the system.
+	defer shutdown()
+
+	// TODO(mattmoor): Set up other important devices.
+
+	// TODO(mattmoor): Set up networking.
+
+	// TODO(mattmoor): Set up the entrypoint/cmd
+	cmd := exec.CommandContext(ctx, "cat", "/proc/cmdline")
+
+	// TODO(mattmoor): Does this even make sense for init?
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	// TODO(mattmoor): Set up the environment.
+	cmd.Env = []string{
+		"PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin",
+	}
+
+	// TODO(mattmoor): Set the user/group to run as.
+	// uid, _ := strconv.Atoi("1000") // Replace with the desired user's UID
+	// gid, _ := strconv.Atoi("1000") // Replace with the desired group's GID
+	// cmd.SysProcAttr = &syscall.SysProcAttr{
+	//     Credential: &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)},
+	// }
+
+	// Run the command, and wait for it to finish.
+	if err := cmd.Run(); err != nil {
+		log.Panicf("failed to run command: %v", err)
+	}
 }
